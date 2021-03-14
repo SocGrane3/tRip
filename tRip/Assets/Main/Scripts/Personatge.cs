9bfa9624d27b2ef0dec5bb8b592cb858995c25a4
Sergi <sergi.granell.7e3@itb.cat>
2021-03-14 22:58:57 +0100

    FUNCIONAAAAA!!!
    he añadido todo lo del personage i de paso he puesto un gameManager que tenia ya creado en la otra branca.

---
+++ tRip/Assets/Main/Scripts/Personatge.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personatge : MonoBehaviour
+{
+    private Vector3 initialPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        initialPosition = this.gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (this.gameObject.transform.position.y < -10) mort();
+    }
+
+    public void mort()
+    {
+        Debug.Log("Mort, position: "+this.gameObject.transform.position.y);
+        this.gameObject.transform.position = initialPosition;
+    }
+}
