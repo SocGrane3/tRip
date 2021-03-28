9d20189653ee918283f4b54ebcc51eb039bd7af7
Sergi <sergi.granell.7e3@itb.cat>
2021-03-28 23:50:44 +0200

    dog model i navMesh
    me faltan cosas del navMesh por poner pero no voy por buen camino asi que voy poniendo el modelo del perro

--- tRip/Assets/Main/Scripts/Perro.cs
+++ tRip/Assets/Main/Scripts/Perro.cs
@@ -9,12 +9,14 @@ public class Perro : MonoBehaviour
     [SerializeField] private NavMeshAgent navigation;
     [SerializeField] private Transform player;
     [SerializeField] private Transform pilota;
+    private Animation animation;
     public bool pilotaCatch;
 
     // Start is called before the first frame update
     void Start()
     {
         navigation = GetComponent<NavMeshAgent>();
+        animation = GetComponent<Animation>();
     }
     // Update is called once per frame
     void Update()
@@ -28,11 +30,13 @@ public class Perro : MonoBehaviour
     {
         navigation.destination = pilota.position;
         navigation.stoppingDistance = 1;
+        animation.Play();
     }
 
     public void irAJugador()
     {
         navigation.destination = player.position;
         navigation.stoppingDistance = 1.5f;
+        animation.Play();
     }
 }
