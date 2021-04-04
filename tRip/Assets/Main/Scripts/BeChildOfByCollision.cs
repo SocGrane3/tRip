50cef88cb9a366867415536b782315af55574c93
Pau <pau.marin.7e2@itb,cat>
2021-04-04 05:01:50 +0200

    Fractals

---
+++ tRip/Assets/Main/Scripts/BeChildOfByCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeChildOfByCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.transform.parent = this.transform;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        collision.transform.parent = null;
+    }
+}
