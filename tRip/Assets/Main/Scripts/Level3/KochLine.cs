f17777023726721e7306aade99d9d15478e43cfd
Pau <pau.marin.7e2@itb,cat>
2021-04-04 05:01:50 +0200

    Fractals y arreglo de la plataforma

---
+++ tRip/Assets/Main/Scripts/Level3/KochLine.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class KochLine : KochGenerator
+{
+    LineRenderer lineRenderer;
+    [Range(0,1)]
+    public float lerpAmount;
+    Vector3[] lerpPosition;
+    public float generateMultiplier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = true;
+        lineRenderer.useWorldSpace = false;
+        lineRenderer.loop = true;
+        lineRenderer.positionCount = position.Length;
+        lineRenderer.SetPositions(position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (generationCount != 0)
+        {
+            for (int i = 0; i < position.Length; i++)
+            {
+                lerpPosition[i] = Vector3.Lerp(position[i], targetPosition[i], lerpAmount);
+            }
+            if (useBezierCurves)
+            {
+                bezierPosition = BezierCurve(lerpPosition, bezierVertexCount);
+                lineRenderer.positionCount = bezierPosition.Length;
+                lineRenderer.SetPositions(bezierPosition);
+            }
+            else
+            {
+                lineRenderer.positionCount = lerpPosition.Length;
+                lineRenderer.SetPositions(lerpPosition);
+            }
+           
+        }
+
+
+        if (Input.GetKeyUp(KeyCode.O))
+        {
+            KochGenerate(targetPosition, true, generateMultiplier);
+            lerpPosition = new Vector3[position.Length];
+            lineRenderer.positionCount = position.Length;
+            lineRenderer.SetPositions(position);
+            lerpAmount = 0;
+        }
+        if (Input.GetKeyUp(KeyCode.I))
+        {
+            KochGenerate(targetPosition, false, generateMultiplier);
+            lerpPosition = new Vector3[position.Length];
+            lineRenderer.positionCount = position.Length;
+            lineRenderer.SetPositions(position);
+            lerpAmount = 0;
+        }
+    }
+}
